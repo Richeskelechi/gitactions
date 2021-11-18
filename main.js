b8c3a6f1e21421f1b13124ccf082476480aeae39
Riches <richeskelechi94.rk@gmail.com>
2021-11-18 13:57:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ function add () {
   const num1 = 5
   const num2 = 3
 
-        // add two numbers
+  // add two numbers
   const sum = num1 + num2
 
-            // display the sum
+  // display the sum
   console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum)
 }
-        // call function
+// call function
 add()
