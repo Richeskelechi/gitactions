b7e2af664e29ca352af55439ea39467586768c30
Riches Kelechi <richeskelechi94.rk@gmail.com>
2021-11-18 14:56:34 +0100

    The new Function Created

---
+++ newMain.js
@@ -0,0 +1,12 @@
+function multiply () {
+  const num1 = 5
+  const num2 = 3
+
+  // multiply two numbers
+  const sum = num1 * num2
+
+  // display the sum
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is: ' + sum)
+}
+        // call function
+multiply()
