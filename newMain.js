5f59c7fda4aa3330f2eead87fe2a4ae5fa96a71a
Riches <richeskelechi94.rk@gmail.com>
2021-11-18 15:02:56 +0100

    Update newMain.js

--- newMain.js
+++ newMain.js
@@ -3,10 +3,10 @@ function multiply () {
   const num2 = 3
 
   // multiply two numbers
-  const sum = num1 * num2
+  const product = num1 * num2
 
   // display the sum
-  console.log('The product of ' + num1 + ' and ' + num2 + ' is: ' + sum)
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is: ' + product)
 }
 // call function
 multiply()
